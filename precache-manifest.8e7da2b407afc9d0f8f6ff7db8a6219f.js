0156742d874b04bef600ad08707038c614ef6db4
bekush <bekushkadiri@gmail.com>
2020-12-19 20:02:44 +0500

    Updates

---
+++ precache-manifest.8e7da2b407afc9d0f8f6ff7db8a6219f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9c18561077bb034a5d60cecbeed7e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247861eb59dbc7632d31",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "e183d3229dc4256aeeaa",
+    "url": "/static/css/main.15b2bc7b.chunk.css"
+  },
+  {
+    "revision": "247861eb59dbc7632d31",
+    "url": "/static/js/2.d0c35b7e.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.d0c35b7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e183d3229dc4256aeeaa",
+    "url": "/static/js/main.07795a2e.chunk.js"
+  },
+  {
+    "revision": "43e8e4bbd3db8886556b",
+    "url": "/static/js/runtime-main.34a32625.js"
+  }
+]);
